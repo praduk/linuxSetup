634f98d4729c678bd3cb65c128952420761285f4
Pradu Kannan <pradu@PraduDebian.pradu.us>
2017-06-24 12:51:04 -0700

    Debian program install script

---
+++ installDebian.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+sudo add-apt-repository -y ppa:george-edison55/cmake-3.x
+sudo apt-get -y update
+
+sudo apt-get -y install build-essential
+sudo apt-get -y install gfortran
+sudo apt-get -y install vim-gnome
+sudo apt-get -y install cmake
+sudo apt-get -y install gtg
+sudo apt-get -y install chromium
+
+sudo apt-get -y upgrade
