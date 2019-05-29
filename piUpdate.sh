30abc5d50092a3d2f54b05fe877a55ad7f7856dc
Pradu Kannan <praduk@gmail.com>
2019-05-29 05:25:48 -0700

    Update piUpdate.sh

--- piUpdate.sh
+++ piUpdate.sh
@@ -1,6 +1,8 @@
+#!/bin/bash
 pullServer /data/music
+pullServer /data/mycroft
 cd ~/linuxSetup
 git pull
-cd /opt/mycroft/skills/praduk-skill.praduk
+cd /opt/mycroft/skills/pradu-skill.praduk/
 git pull
-reboot
+#reboot
