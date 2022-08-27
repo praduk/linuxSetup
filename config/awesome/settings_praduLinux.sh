e2bb704f2cc6d0947ed212aca3c770d96be121b5
Pradu Kannan <pradukan@amazon.com>
2022-08-27 05:58:59 -0700

    background setting

--- config/awesome/settings_praduLinux.sh
+++ config/awesome/settings_praduLinux.sh
@@ -5,3 +5,6 @@ xrandr --output HDMI-0 --mode 3840x2160 --rate 120
 
 # Audio Setup
 pacmd set-default-sink alsa_output.usb-Generic_USB_Audio-00.analog-stereo 
+
+# Background
+feh --bg-scale backgrounds/kuiper.png
