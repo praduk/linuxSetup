3a3af027d7ae94e6428449407b4af09bd2c198dd
Pradu Kannan <pradukan@amazon.com>
2022-09-21 05:15:30 -0700

    updates

--- config/awesome/settings_u34b159a681b853.ant.amazon.com.sh
+++ config/awesome/settings_u34b159a681b853.ant.amazon.com.sh
@@ -4,7 +4,8 @@
 # xrandr --output HDMI-0 --mode 3840x2160 --rate 120
 
 # Audio Setup
-# pacmd set-default-sink alsa_output.usb-Generic_USB_Audio-00.analog-stereo 
+pacmd set-default-sink alsa_output.usb-Generic_USB_Audio-00.analog-stereo 
+pacmd set-default-source alsa_input.usb-046d_HD_Pro_Webcam_C920_5836B57F-02.analog-stereo
 
 # Background
 feh --bg-scale ${HOME}/.config/awesome/backgrounds/kuiper.png
--- config/nvim/lua/custom/plugins/lspconfig.lua
+++ config/nvim/lua/custom/plugins/lspconfig.lua
@@ -4,7 +4,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 local utils = require("core.utils")
 
 local lspconfig = require "lspconfig"
-local servers = { "html", "cssls", "clangd", "yamlls", "julials", "bashls", "jsonls", "pylsp", "cmake", "dotls", "rust_analyzer", "texlab", "ltex", "tsserver"}
+local servers = { "html", "cssls", "yamlls", "julials", "bashls", "jsonls", "pylsp", "cmake", "dotls", "rust_analyzer", "texlab", "ltex", "tsserver"}
 
 
 for _, lsp in ipairs(servers) do
@@ -13,3 +13,9 @@ for _, lsp in ipairs(servers) do
     capabilities = capabilities,
   }
 end
+
+lspconfig["clangd"].setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = {"clangd","--background-index","--suggest-missing-includes","--clang-tidy=false"},
+}
